5e3a13379e196589b8b5e5d95b29aede5a8f48ee
Aleksey <belovnow@gmail.com>
2020-10-02 18:06:24 +0300

    Добавил верное отражение ошибки

--- BookManagerBeta/Models/Book.cs
+++ BookManagerBeta/Models/Book.cs
@@ -8,14 +8,14 @@ namespace BookManagerBeta.Models
 {
     public class Book
     {
-        [Required(ErrorMessage = "Please enter book's title!")]
+        [Required(ErrorMessage = "Please enter book's title")]
         public string Title { get; set; }
-        [Required(ErrorMessage = "Please enter book's author!")]
+        [Required(ErrorMessage = "Please enter book's author")]
         public string Author { get; set; }
-        [Required(ErrorMessage = "Please enter book's description!")]
+        [Required(ErrorMessage = "Please enter book's description")]
         public string Description { get; set; }
-        [Required(ErrorMessage = "Please enter book's number of pages!")]
-        public ushort Pages { get; set; }
+        [Required(ErrorMessage = "Please enter book's number of pages")]
+        public ushort? Pages { get; set; }
         //public int PagesRead { get; set; } = 0;
     }
 }
